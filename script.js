a51f0753858a7dae2deac804c71046a52452f2f6
Merav <merav.mi@gmail.com>
2019-05-22 15:40:53 +0300

    added load events

---
+++ script.js
@@ -0,0 +1,26 @@
+// $("main").load("main.html");
+
+
+$(function () {
+
+    $(function () {
+        $("main").load("main.html");
+    })
+
+    $("#brand-name").click(function () {
+        $("main").load("main.html");
+    })
+
+    $("#l-about").click(function () {
+        $("main").load("about.html");
+    })
+
+    $("#l-contact").click(function () {
+        $("main").load("contact.html");
+    })
+
+});
+
+
+
+
